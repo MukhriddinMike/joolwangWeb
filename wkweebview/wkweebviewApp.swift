bcd2dcaba38bbe907b5316cd44bf99afe6d1967d
Mukhriddin Mike <37968013+MukhriddinMike@users.noreply.github.com>
2021-10-18 08:25:46 +0900

    Initial Commit

---
+++ wkweebview/wkweebviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  wkweebviewApp.swift
+//  wkweebview
+//
+//  Created by Mike on 2021/10/18.
+//
+
+import SwiftUI
+
+@main
+struct wkweebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
